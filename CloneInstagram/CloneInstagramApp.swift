66772d7a809669a89724b3f27900a51822a2f5b6
Matheus de Sousa Matos <69324938+Matheus-Sousa-Matos@users.noreply.github.com>
2024-05-14 10:00:59 -0300

    Initial Commit

---
+++ CloneInstagram/CloneInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneInstagramApp.swift
+//  CloneInstagram
+//
+//  Created by Matheus de Sousa Matos on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CloneInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloneInstagram/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CloneInstagram
+//
+//  Created by Matheus de Sousa Matos on 14/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
